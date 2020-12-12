b6167c32b412cb2933e2584c26c2a1a9afcb0d42
BigSethi <akul.sethi@gmail.com>
2020-12-12 12:43:23 -0500

    Input data pipeline using tf dataset

--- image_classification.py
+++ image_classification.py
@@ -4,53 +4,54 @@ import tensorflow.keras as keras
 import tensorflow.keras.layers as layers
 import time
 
+# Paths
 FEATURES_PATH = 'train_tp_specs.npy'
 LABELS_PATH = 'train_tp_labels.npy'
+
+# Load files
+labels_file = np.load(LABELS_PATH, mmap_mode='r')
+features_file = np.load(FEATURES_PATH, mmap_mode='r')
 
 # Hyper parameters
 BATCH_SIZE = 32
 LEARNING_RATE = 0.01
+SHUFFLE_SIZE = 100
 EPOCHS = 5
 
-
-# Data Processing methods
-def load_batch(batch_size):
-    labels_file = np.load(LABELS_PATH, mmap_mode='r').tolist()
-    features_file = np.load(FEATURES_PATH, mmap_mode='r')
-    random_indexes = np.random.rand(batch_size) * (features_file.shape[0] - 1)
-    random_indexes = np.around(random_indexes).astype('int64')
-    num_batches = round(features_file.shape[0] / batch_size)
-    for n in range(num_batches):
-        batch_features = []
-        batch_labels = []
-        for index in random_indexes:
-            shape = features_file[index].shape
-            batch_features.append(features_file[index].reshape(shape[0], shape[1], 1))
-            batch_labels.append(labels_file[index])
-
-        yield np.stack(batch_features), np.stack(batch_labels)
+spec_shape = (features_file.shape[1], features_file.shape[2], 1)
 
 
-def normalize(spectogram):
-    result = (spectogram + 73.864) / (52.06553 + 73.864)
-    return result
+# Spectrogram size is (1025, 2584, 1)
+
+def generator():
+    for i, f in enumerate(features_file):
+        yield f, labels_file[i]
 
 
-a = np.load(FEATURES_PATH, mmap_mode='r')[1]
-spec_shape = (a.shape[0], a.shape[1], 1)
+def normalize(*args):
+    new_spec = (args[0] + 73.864) / (52.06553 + 73.864)
+    new_spec = tf.reshape(new_spec, [BATCH_SIZE, spec_shape[0], spec_shape[1], spec_shape[2]])
+    return new_spec, args[1]
+
 
 # Max value is 52.06553
 # Min value is -73.864
-# Spectrogram size is (1025, 2584, 1)
 
+
+train_ds = tf.data.Dataset.from_generator(generator, output_types=(tf.float64, tf.int32),
+                                          output_shapes=((1025, 2584), ()))
+train_ds = train_ds.shuffle(buffer_size=SHUFFLE_SIZE)
+train_ds = train_ds.batch(BATCH_SIZE, drop_remainder=True)
+train_ds = train_ds.map(normalize, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+train_ds = train_ds.prefetch(tf.data.experimental.AUTOTUNE)
 
 model = keras.Sequential([
     layers.Input(shape=spec_shape),
-    layers.Conv2D(64, (4, 4), activation='relu'),
+    layers.MaxPool2D(pool_size=(15, 15)),
+    layers.Conv2D(16, (4, 4), activation='relu'),
     layers.MaxPool2D(),
-    layers.Conv2D(64, (4, 4), activation='relu'),
+    layers.Conv2D(32, (4, 4), activation='relu'),
     layers.MaxPool2D(),
-    layers.Conv2D(64, (4, 4), activation='relu'),
     layers.Flatten(),
     layers.Dense(128, activation='relu'),
     layers.Dense(24, activation='softmax')
@@ -61,10 +62,4 @@ model.compile(optimizer=optimizer, loss=keras.losses.sparse_categorical_crossent
 model.summary()
 
 # Fit model
-for e in range(EPOCHS):
-    for features, labels in load_batch(BATCH_SIZE):
-        features = normalize(features)
-        model.fit(features, labels, batch_size=BATCH_SIZE, epochs=1)
-
-
-
+history = model.fit(train_ds, epochs=EPOCHS)
