Fix softmax issue

image_classification.py
@@ -1,60 +1,110 @@
 import numpy as np
+import librosa as lb
+import librosa.display
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+import pandas as pd
 import tensorflow as tf
 import tensorflow.keras as keras
 import tensorflow.keras.layers as layers
 import os
+import math
 import time
 
 # Paths
-DATA_PATH = 'results'
-
-# Load files
+TRAIN_DATA_PATH = 'results'
+TEST_DATA_PATH = 'archive'
 
 # Hyper parameters
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 LEARNING_RATE = 0.01
 SHUFFLE_SIZE = 100
-EPOCHS = 5
+EPOCHS = 1
 
 
-# Spectrogram size is (1025, 2584, 1)
+# Spectrogram size is (128, 500, 1)
+# Number of train files is about 32*36
+# Number of test files is 1992
 
-def generator():
-    for root, dirnames, filenames in os.walk(DATA_PATH):
-        for file in filenames:
+def train_generator(start, end):
+    for root, dirnames, filenames in os.walk(TRAIN_DATA_PATH):
+        for file in filenames[start:end]:
             split = file.split('_')
             label = split[1].split('.')[0]
-            yield np.load(os.path.join(DATA_PATH, file)), label
+            yield np.load(os.path.join(TRAIN_DATA_PATH, file)), label
 
+
+def test_generator():
+    specs = []
+    for root, dirnames, filenames in os.walk(TEST_DATA_PATH):
+        for file in filenames:
+            specs.append(np.load(os.path.join(TEST_DATA_PATH, file)))
+    return np.concatenate(specs)
+
+
+test_ds = test_generator()
 
 spec_shape = (128, 500, 1)
 
-train_ds = tf.data.Dataset.from_generator(generator, output_types=(tf.float64, tf.int8),
-                                          output_shapes=(spec_shape, ()))
+sample_size = 10
+stride_size = 10
+num_specs_in_sample = math.floor((60 - sample_size) / stride_size) + 1
+
+train_ds = tf.data.Dataset.from_generator(train_generator, output_types=(tf.float64, tf.int8),
+                                          output_shapes=(spec_shape, ()), args=(tf.constant(0), tf.constant(800)))
 
 train_ds = train_ds.cache().shuffle(buffer_size=SHUFFLE_SIZE)
 train_ds = train_ds.batch(BATCH_SIZE, drop_remainder=True)
 train_ds = train_ds.prefetch(tf.data.experimental.AUTOTUNE)
+
+val_ds = tf.data.Dataset.from_generator(train_generator, output_types=(tf.float64, tf.int8),
+                                        output_shapes=(spec_shape, ()), args=(tf.constant(800), tf.constant(-1)))
+val_ds = val_ds.cache().batch(BATCH_SIZE, drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE)
 
 norm_layer = keras.layers.experimental.preprocessing.Normalization()
 norm_layer.adapt(train_ds.map(lambda x, _: x))
 
 model = keras.Sequential([
     layers.Input(shape=spec_shape),
-    layers.Conv2D(16, (3, 3), activation='relu'),
     norm_layer,
+    layers.Conv2D(16, (3, 3), activation='relu'),
     layers.MaxPool2D(),
     layers.Conv2D(32, (3, 3), activation='relu'),
     layers.MaxPool2D(),
+    layers.Dropout(0.2),
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.Flatten(),
     layers.Dense(128, activation='relu'),
+    layers.Dropout(0.2),
     layers.Dense(24, activation='softmax')
 ])
 
-optimizer = keras.optimizers.Adam(learning_rate=LEARNING_RATE)
-model.compile(optimizer=optimizer, loss=keras.losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+model.compile(keras.optimizers.Adam(), loss=keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
 model.summary()
 
 # Fit model
-model.fit(train_ds, epochs=EPOCHS)
+model.fit(train_ds, epochs=EPOCHS, validation_data=val_ds)
+# model.load_weights('saved_model/')
+predictions = model.predict(test_ds)
+print(predictions)
+print(predictions.shape)
+
+predictions = np.reshape(predictions, (-1, num_specs_in_sample, 24))
+print(predictions)
+print('Done with submissions')
+submission = np.max(predictions, axis=1)
+print(submission)
+
+columns = ['s0', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12',
+           's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21', 's22', 's23']
+
+dataframe = pd.DataFrame(submission, columns=columns)
+
+for dirname, _, filelist in os.walk(TEST_DATA_PATH):
+    files = []
+    for file in filelist:
+        files.append(file.split('.')[0])
+    dataframe.insert(0, 'recording_id', files)
+
+dataframe.to_csv('submission.csv', index=False)

preProcessing.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import os
 
 TRAIN_PATH = "rfcx-species-audio-detection/train/"
+TEST_DATA_PATH = 'rfcx-species-audio-detection/test'
 
 train_tp = pd.read_csv("rfcx-species-audio-detection/train_tp.csv", delimiter=',')
 train_fp = pd.read_csv("rfcx-species-audio-detection/train_fp.csv", delimiter=',')
@@ -80,7 +81,31 @@ def prep_mel_specs(sr, n_fft, hop_length):
         np.save(row['recording_id'] + '_' + row['species_id'], mel_spec)
 
 
-prep_mel_specs(48000, 2048, 512)
+def prep_test_specs(sample_length, stride):
+    n_fft = 2048
+    hop_length = 512
+    min_fr = 93.75 * 0.9
+    max_fr = 13687.5 * 1.1
+    root, dirnames, filenames = next(iter(os.walk(TEST_DATA_PATH)))
+    for file in filenames:
+        spec_batch = []
+        signal, sr = lb.load(os.path.join(TEST_DATA_PATH, file), sr=None)
+        start = 0
+        end = int(sample_length * sr)
+        while end <= signal.shape[0]:
+            signal_bite = signal[start:end]
+            new_spec = lb.feature.melspectrogram(signal_bite, sr=sr, n_fft=n_fft, hop_length=hop_length,
+                                                 fmin=min_fr, fmax=max_fr, power=1.5)
+            new_spec = resize(new_spec, (128, 500))
+            new_spec = np.reshape(new_spec, (new_spec.shape[0], new_spec.shape[1], 1))
+            spec_batch.append(new_spec)
+
+            start += int(stride * sr)
+            end = int(start + sample_length * sr)
+        np.save(file, np.stack(spec_batch))
+
+
+prep_test_specs(10,10)
 
 # 1 hour to process all data
 # 12.44 GB for all data